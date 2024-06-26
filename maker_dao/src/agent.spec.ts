d2d2ce615caf247087959af4b9cd7637ca1312ef
vicsnet <adesanmiadebisi@gmail.com>
2024-06-26 15:08:18 +0100

    feat: Agent Implementation

--- maker_dao/src/agent.spec.ts
+++ maker_dao/src/agent.spec.ts
@@ -6,69 +6,95 @@ import {
   createTransactionEvent,
   ethers,
 } from "forta-agent";
-import agent, {
-  ERC20_TRANSFER_EVENT,
-  TETHER_ADDRESS,
-  TETHER_DECIMALS,
-} from "./agent";
+import { TestTransactionEvent } from "forta-agent-tools/lib/test";
+import {
+  L1_DAI_CONTRACT_ADDRESS,
+  L1_ESCROW_ADDRESS_OPTIMISM,
+  L1_ESCROW_ADDRESS_ARBITRUM,
+  L2_DAI_OPTIMISM,
+  L2_DAI_ARBITRUM,
+  L2_DAI_GATEWAY_ARB,
+  L1_AARBITRUM_GATEWAY,
+  PROVIDER_EThereum,
+  PROVIDER_OP,
+  PROVIDER_ARBI,
+  DEPOSIT_FINALISED_EVENT,
+  GET_TOTAL_SUPPLY,
+  TRANSFER_EVENT
+} from "./constant";
+import agent from "./agent";
+import { createAddress } from "forta-agent-tools";
 
-describe("high tether transfer agent", () => {
-  let handleTransaction: HandleTransaction;
-  const mockTxEvent = createTransactionEvent({} as any);
+describe("Dai briged", () => {
+    let { handleTransaction: arbTransaction } = agent(
+      PROVIDER_ARBI,
+      L2_DAI_ARBITRUM,
+      "",
+      ""
+    );
+    let { handleTransaction: opTransaction } = agent(
+      PROVIDER_OP,
+      L2_DAI_ARBITRUM,
+      "",
+      ""
+    );
+    let { handleTransaction: ethTransaction } = agent(
+      PROVIDER_EThereum,
+      L2_DAI_ARBITRUM,
+      "1",
+      "4"
+    );
+  let txEvent: TestTransactionEvent;
 
+
+  
   beforeAll(() => {
-    handleTransaction = agent.handleTransaction;
+    jest.spyOn(Date, "now").mockImplementation(() => 1590000000000);
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
   });
 
-  describe("handleTransaction", () => {
-    it("returns empty findings if there are no Tether transfers", async () => {
-      mockTxEvent.filterLog = jest.fn().mockReturnValue([]);
+  const from = createAddress("0x3");
+  const l1Token = createAddress("0x344");
+  const to = L1_AARBITRUM_GATEWAY;
+  const address = "0x61D3f523cd7e93d8deF89bb5d5c4eC178f7CfE76".toLowerCase();
+  it("returns transaction calls for deposit finalised result", async () => {
+    txEvent = new TestTransactionEvent().addEventLog(
+      DEPOSIT_FINALISED_EVENT,
+      address,
+      [l1Token, from, to, 10]
+    );
 
-      const findings = await handleTransaction(mockTxEvent);
-
-      expect(findings).toStrictEqual([]);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-        ERC20_TRANSFER_EVENT,
-        TETHER_ADDRESS
-      );
-    });
-
-    it("returns a finding if there is a Tether transfer over 10,000", async () => {
-      const mockTetherTransferEvent = {
-        args: {
-          from: "0xabc",
-          to: "0xdef",
-          value: ethers.BigNumber.from("20000000000"), //20k with 6 decimals
+    console.log('test',txEvent);
+    
+    const findings = await arbTransaction(txEvent);
+    const totalSupply = await GET_TOTAL_SUPPLY(L2_DAI_ARBITRUM, PROVIDER_ARBI);
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: `Deposit occur on 42161 bridge`,
+        description: `h1`,
+        alertId: "FORTA-1",
+        severity: FindingSeverity.Low,
+        type: FindingType.Info,
+        metadata: {
+          totalSupply: totalSupply.toString(),
+          amount: "10",
+          from: from,
+          to: to,
         },
-      };
-      mockTxEvent.filterLog = jest
-        .fn()
-        .mockReturnValue([mockTetherTransferEvent]);
-
-      const findings = await handleTransaction(mockTxEvent);
-
-      const normalizedValue = mockTetherTransferEvent.args.value.div(
-        10 ** TETHER_DECIMALS
-      );
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to: mockTetherTransferEvent.args.to,
-            from: mockTetherTransferEvent.args.from,
-          },
-        }),
-      ]);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-        ERC20_TRANSFER_EVENT,
-        TETHER_ADDRESS
-      );
-    });
+      }),
+    ]);
+   
   });
+  it("calls the eth transaction and returns empty findings", async()=>{
+    txEvent = new TestTransactionEvent().addEventLog(TRANSFER_EVENT, address, [
+        from,
+        to,
+        10
+    ]);
+
+    const findings = await ethTransaction(txEvent);
+    expect(findings).toStrictEqual([]);
+})
 });
--- maker_dao/src/agent.ts
+++ maker_dao/src/agent.ts
@@ -7,86 +7,182 @@ import {
   HandleTransaction,
   HandleAlert,
   AlertEvent,
+  Alert,
   TransactionEvent,
   FindingSeverity,
   FindingType,
+  getJsonRpcUrl,
+  ethers,
+  getAlerts,
 } from "forta-agent";
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
-let findingsCount = 0;
+import {
+  L1_DAI_CONTRACT_ADDRESS,
+  L1_ESCROW_ADDRESS_OPTIMISM,
+  L1_ESCROW_ADDRESS_ARBITRUM,
+  L2_DAI_OPTIMISM,
+  L2_DAI_ARBITRUM,
+  L2_DAI_GATEWAY_ARB,
+  L1_AARBITRUM_GATEWAY,
+  PROVIDER_EThereum,
+  PROVIDER_OP,
+  PROVIDER_ARBI,
+  TRANSFER_EVENT,
+  DEPOSIT_FINALISED_EVENT,
+  ETHER_CHAINID,
+  OP_CHAINID,
+  ARBI_CHAINID,
+  GET_DAI_BALANCE,
+  GET_TOTAL_SUPPLY,
+} from "./constant";
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
+const provideHandleTransaction =
+  (
+    provider: ethers.providers.JsonRpcProvider,
+    contractAddr: string,
+    botId: string,
+    alertId: string
+  ) =>
+  async (txEvent: TransactionEvent) => {
+    let findings: Finding[] = [];
+    const transferEvent = txEvent.filterLog(TRANSFER_EVENT);
+    console.log("event", txEvent);
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+    const network = await provider.getNetwork();
+    // read the balance of l1 escrow
+    const chainId = network.chainId;
+    const DepositEvent = txEvent.filterLog(DEPOSIT_FINALISED_EVENT);
 
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
-  );
+    if (chainId !== 1) {
+      DepositEvent.forEach(async (deposit) => {
+        const { l1Token, from, to, amount } = deposit.args;
 
-  tetherTransferEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
-
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
-      findings.push(
-        Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to,
-            from,
-          },
-        })
-      );
-      findingsCount++;
+        if (chainId === OP_CHAINID || chainId === ARBI_CHAINID) {
+          const data = await GET_TOTAL_SUPPLY(contractAddr, provider);
+          console.log(chainId);
+          console.log("total supply", data.toString());
+          findings.push(
+            Finding.fromObject({
+              name: `Deposit occur on ${chainId} bridge`,
+              description: `h1`,
+              alertId: "FORTA-1",
+              severity: FindingSeverity.Low,
+              type: FindingType.Info,
+              metadata: {
+                totalSupply: data.toString(),
+                amount: amount.toString(),
+                from: from,
+                to: to,
+              },
+            })
+          );
+        }
+      });
     }
-  });
 
-  return findings;
-};
+    if (chainId === 1) {
+      console.log("ch", chainId);
 
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
+     
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
 
-// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some alert condition
-//   return findings;
-// }
 
-// const healthCheck: HealthCheck = async () => {
-//   const errors: string[] = [];
-  // detect some health check condition
-  // errors.push("not healthy due to some condition")
-  // return errors;
-// }
+      // console.log("al", JSON.stringify(alerts));
+      const to_Adrress = txEvent.to;
+      console.log("to_Adrress", txEvent.gasPrice);
 
-export default {
-  // initialize,
-  handleTransaction,
-  // healthCheck,
-  // handleBlock,
-  // handleAlert
-};
+      console.log("helo1");
+      if (txEvent.to?.toLowerCase() === L1_AARBITRUM_GATEWAY.toLowerCase()) {
+        console.log("event", txEvent.to.toString());
+        const { alerts } = await getAlerts({
+          botIds: [botId],
+          alertId: alertId,
+          chainId: chainId,
+          first: 1,
+        });
+        transferEvent.forEach(async (e) => {
+          if (alerts) {
+            const totalSupply = await alerts[0].metadata.totalSupply;
+
+            const Escrowbalance = await GET_DAI_BALANCE(
+              provider,
+              L1_ESCROW_ADDRESS_ARBITRUM
+            );
+
+            console.log("my Escrow", Escrowbalance);
+            if (Escrowbalance >= totalSupply) {
+              console.log("tSupply", totalSupply);
+              return findings;
+            } else {
+              findings.push(
+                Finding.fromObject({
+                  name: `Deposit occur on ${chainId} bridge`,
+                  description: ``,
+                  alertId: "FORTA-1",
+                  severity: FindingSeverity.High,
+                  type: FindingType.Suspicious,
+                  metadata: {
+                    totalSupply: totalSupply.toString(),
+                    balance: Escrowbalance.toString(),
+                  },
+                })
+              );
+            }
+          }
+        });
+      }
+
+      if (txEvent.to === L1_ESCROW_ADDRESS_OPTIMISM) {
+        const { alerts } = await getAlerts({
+          botIds: [botId],
+          alertId: alertId,
+          chainId: chainId,
+          first: 1,
+        });
+        transferEvent.forEach((e) => {
+          if (alerts) {
+            const totalSupply = alerts[0].metadata.totalSupply;
+
+            const Escrowbalance = GET_DAI_BALANCE(
+              provider,
+              L1_ESCROW_ADDRESS_OPTIMISM
+            );
+
+            console.log(Escrowbalance);
+            if (Escrowbalance >= totalSupply) {
+              return findings;
+            } else {
+              findings.push(
+                Finding.fromObject({
+                  name: `Deposit occur on ${chainId} bridge`,
+                  description: ``,
+                  alertId: "FORTA-1",
+                  severity: FindingSeverity.High,
+                  type: FindingType.Suspicious,
+                  metadata: {
+                    totalSupply: totalSupply.toString(),
+                    balance: Escrowbalance.toString(),
+                  },
+                })
+              );
+            }
+          }
+        });
+      }
+    }
+    return findings;
+  };
+
+export default (
+  provider: ethers.providers.JsonRpcProvider,
+  contractAddr: string,
+  botId: string,
+  alertId: string
+) => ({
+  handleTransaction: provideHandleTransaction(
+    provider,
+    contractAddr,
+    botId,
+    alertId
+  ),
+});
